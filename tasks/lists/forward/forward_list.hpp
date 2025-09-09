de010a9d58b460c4ccaae3f2aa24af1d3a07d375
Mafonchik <nikoloztodua56816@gmail.com>
2025-09-09 15:46:43 +0300

    Update forward_list.hpp

--- tasks/lists/forward/forward_list.hpp
+++ tasks/lists/forward/forward_list.hpp
@@ -2,11 +2,26 @@
 
 #include <fmt/core.h>
 
+#include <algorithm>
 #include <cstdlib>
 #include <functional>
+#include <initializer_list>
 #include <iterator>
-#include <utility>
 #include <stdexcept>
+#include <utility>
+
+class ListIsEmptyException : public std::exception {
+public:
+    explicit ListIsEmptyException(const std::string& text = "List is empty") : error_message_(text) {
+    }
+
+    const char* what() const noexcept override {
+        return error_message_.c_str();
+    }
+
+private:
+    std::string error_message_;
+};
 
 template <typename T>
 class ForwardList {
@@ -19,59 +34,72 @@ private:
         T value_;
         Node* next_;
 
-        explicit Node(const T& value, Node* next = nullptr)
-            : value_(value), next_(next) {}
+        explicit Node(const T& val, Node* nxt = nullptr) : value_(val), next_(nxt) {
+        }
+        explicit Node(T&& val, Node* nxt = nullptr) : value_(std::move(val)), next_(nxt) {
+        }
     };
 
 public:
     class ForwardListIterator {
     public:
-        using IteratorCategory = std::forward_iterator_tag;
-        using ValueType = T;
-        using DifferenceType = std::ptrdiff_t;
-        using Pointer = T*;
-        using Reference = T&;
+        using iterator_category = std::forward_iterator_tag;  // NOLINT
+        using value_type = T;                                 // NOLINT
+        using difference_type = std::ptrdiff_t;               // NOLINT
+        using pointer = T*;                                   // NOLINT
+        using reference = T&;                                 // NOLINT
 
-        ForwardListIterator() : current_(nullptr) {}
-        
-        Reference operator*() const {
+        ForwardListIterator() : current_(nullptr) {
+        }
+
+        reference operator*() const {
+            if (current_ == nullptr) {
+                throw std::runtime_error("Dereferencing end iterator");
+            }
             return current_->value_;
         }
-        
-        Pointer operator->() const {
+
+        pointer operator->() const {
+            if (current_ == nullptr) {
+                throw std::runtime_error("Dereferencing end iterator");
+            }
             return &current_->value_;
         }
-        
+
         ForwardListIterator& operator++() {
-            current_ = current_->next_;
+            if (current_ != nullptr) {
+                current_ = current_->next_;
+            }
             return *this;
         }
-        
+
         ForwardListIterator operator++(int) {
             ForwardListIterator temp = *this;
             ++(*this);
             return temp;
         }
-        
+
         bool operator==(const ForwardListIterator& other) const {
             return current_ == other.current_;
         }
-        
+
         bool operator!=(const ForwardListIterator& other) const {
             return !(*this == other);
         }
 
     private:
-        explicit ForwardListIterator(Node* node) : current_(node) {}
+        explicit ForwardListIterator(Node* node) : current_(node) {
+        }
 
     private:
         Node* current_;
-        
+
         friend class ForwardList;
     };
 
 public:
-    ForwardList() : head_(nullptr), size_(0) {}
+    ForwardList() : head_(nullptr), size_(0) {
+    }
 
     explicit ForwardList(size_t sz) : head_(nullptr), size_(0) {
         for (size_t i = 0; i < sz; ++i) {
@@ -81,24 +109,23 @@ public:
 
     ForwardList(const std::initializer_list<T>& values) : head_(nullptr), size_(0) {
         for (auto it = values.end(); it != values.begin();) {
-            PushFront(*--it);
+            --it;
+            PushFront(*it);
         }
     }
 
     ForwardList(const ForwardList& other) : head_(nullptr), size_(0) {
         if (!other.IsEmpty()) {
+            ForwardList temp;
             Node* other_current = other.head_;
-            Node* new_head = new Node(other_current->value_);
-            head_ = new_head;
-            Node* current = head_;
-            size_ = 1;
-            
-            other_current = other_current->next_;
             while (other_current != nullptr) {
-                current->next_ = new Node(other_current->value_);
-                current = current->next_;
+                temp.PushFront(other_current->value_);
                 other_current = other_current->next_;
-                ++size_;
+            }
+
+            while (!temp.IsEmpty()) {
+                PushFront(temp.Front());
+                temp.PopFront();
             }
         }
     }
@@ -107,25 +134,22 @@ public:
         if (this != &other) {
             Clear();
             if (!other.IsEmpty()) {
+                ForwardList temp;
                 Node* other_current = other.head_;
-                Node* new_head = new Node(other_current->value_);
-                head_ = new_head;
-                Node* current = head_;
-                size_ = 1;
-                
-                other_current = other_current->next_;
                 while (other_current != nullptr) {
-                    current->next_ = new Node(other_current->value_);
-                    current = current->next_;
+                    temp.PushFront(other_current->value_);
                     other_current = other_current->next_;
-                    ++size_;
+                }
+
+                while (!temp.IsEmpty()) {
+                    PushFront(temp.Front());
+                    temp.PopFront();
                 }
             }
         }
         return *this;
     }
-
-    ForwardListIterator Begin() const noexcept {
+ForwardListIterator Begin() const noexcept {
         return ForwardListIterator(head_);
     }
 
@@ -135,7 +159,7 @@ public:
 
     inline T& Front() const {
         if (IsEmpty()) {
-            throw std::out_of_range("List is empty");
+            throw ListIsEmptyException("List is empty");
         }
         return head_->value_;
     }
@@ -157,11 +181,11 @@ public:
         if (pos.current_ == nullptr || pos.current_->next_ == nullptr) {
             return;
         }
-        
+
         Node* to_delete = pos.current_->next_;
         pos.current_->next_ = to_delete->next_;
         delete to_delete;
-        --size_;
+        size_--;
     }
 
     void InsertAfter(ForwardListIterator pos, const T& value) {
@@ -169,10 +193,10 @@ public:
             PushFront(value);
             return;
         }
-        
+
         Node* new_node = new Node(value, pos.current_->next_);
         pos.current_->next_ = new_node;
-        ++size_;
+        size_++;
     }
 
     ForwardListIterator Find(const T& value) const {
@@ -187,26 +211,35 @@ public:
     }
 
     void Clear() noexcept {
-        while (!IsEmpty()) {
-            PopFront();
+        Node* current = head_;
+        while (current != nullptr) {
+            Node* next = current->next_;
+            delete current;
+            current = next;
         }
+        head_ = nullptr;
+        size_ = 0;
     }
 
     void PushFront(const T& value) {
-        Node* new_node = new Node(value, head_);
-        head_ = new_node;
-        ++size_;
+        head_ = new Node(value, head_);
+        size_++;
+    }
+
+    void PushFront(T&& value) {
+        head_ = new Node(std::move(value), head_);
+        size_++;
     }
 
     void PopFront() {
         if (IsEmpty()) {
-            throw std::out_of_range("List is empty");
+            throw ListIsEmptyException("List is empty");
         }
-        
+
         Node* to_delete = head_;
         head_ = head_->next_;
         delete to_delete;
-        --size_;
+        size_--;
     }
 
     ~ForwardList() {
@@ -219,7 +252,6 @@ private:
 };
 
 namespace std {
-// Global swap overloading
 template <typename T>
 void swap(ForwardList<T>& a, ForwardList<T>& b) {  // NOLINT
     a.Swap(b);
