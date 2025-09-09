018a19f6916e77eda382a8a6a8d41271f6f8f33c
Mafonchik <nikoloztodua56816@gmail.com>
2025-09-09 16:34:06 +0300

    Update vector.hpp

--- tasks/vector/vector/vector.hpp
+++ tasks/vector/vector/vector.hpp
@@ -1,34 +1,47 @@
 #pragma once
-#include <cstddef>
-#include <initializer_list>
-#include <utility>
 
+#include <initializer_list>
+#include <mutex>
+#include <stdexcept>
+#include <utility>
 template <typename T>
 class Vector {
 public:
     Vector();
+
     Vector(size_t count, const T& value);
+
     Vector(const Vector& other);
+
     Vector& operator=(const Vector& other);
+
     Vector(Vector&& other) noexcept;
+
     Vector& operator=(Vector&& other);
+
     Vector(std::initializer_list<T> init);
 
-    T& operator[](size_t position);
+    T& operator[](size_t pos);
 
     T& Front() const noexcept;
+
     T& Back() const noexcept;
+
     T* Data() const noexcept;
 
     bool IsEmpty() const noexcept;
+
     size_t Size() const noexcept;
+
     size_t Capacity() const noexcept;
 
-    void Reserve(size_t new_capacity);
+    void Reserve(size_t new_cap);
+
     void Clear() noexcept;
 
-    void Insert(size_t position, T value);
-    void Erase(size_t begin_position, size_t end_position);
+    void Insert(size_t pos, T value);
+
+    void Erase(size_t begin_pos, size_t end_pos);
 
     void PushBack(T value);
 
@@ -36,13 +49,16 @@ public:
     void EmplaceBack(Args&&... args);
 
     void PopBack();
+
     void Resize(size_t count, const T& value);
 
     ~Vector();
 
 private:
-    static constexpr size_t InitialCapacity = 10;  // Changed from kInitialCapacity to InitialCapacity
-    mutable T* data_;
-    size_t size_;
-    size_t capacity_;
+    T* data_ = nullptr;
+    size_t size_ = 0;
+    size_t capacity_ = 0;
+    static constexpr size_t DefaultCapacity = 10;
+    mutable std::mutex mutex_;
+    void Reallocate(size_t new_cap);
 };
