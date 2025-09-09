b9f3fd0999e17b49b0c1f56efb6ac7e9b980450f
Mafonchik <nikoloztodua56816@gmail.com>
2025-09-09 17:02:49 +0300

    Update directory.cpp

--- tasks/tree/NTree/filesystem/files/directory.cpp
+++ tasks/tree/NTree/filesystem/files/directory.cpp
@@ -1,3 +1,28 @@
 #include "directory.hpp"
 
-namespace filesystem {}  // end namespace filesystem
+#include <iostream>
+
+namespace filesystem {
+
+Directory::Directory(Directory* parent) : parent_(parent) {
+}
+
+Directory::~Directory() {
+    for (auto& [name, dir] : childs_.Values()) {
+        delete dir;
+    }
+}
+
+void Directory::GetName() const {
+    if (parent_) {
+        for (const auto& [name, dir] : parent_->childs_.Values()) {
+            if (dir == this) {
+                std::cout << name << std::endl;
+                return;
+            }
+        }
+    }
+    std::cout << "(root)" << std::endl;
+}
+
+}  // namespace filesystem
