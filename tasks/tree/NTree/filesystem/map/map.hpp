63e782cdf838895b2f929c46ad31e7ae1cbe226b
Mafonchik <nikoloztodua56816@gmail.com>
2025-09-09 15:51:09 +0300

    Update map.hpp

--- tasks/tree/NTree/filesystem/map/map.hpp
+++ tasks/tree/NTree/filesystem/map/map.hpp
@@ -1,81 +1,102 @@
 #pragma once
-
-#include <cstdlib>
-#include <functional>
+#include <initializer_list>
 #include <utility>
 #include <vector>
 
 template <typename Key, typename Value, typename Compare = std::less<Key>>
 class Map {
 public:
-    Map() {
-        // Not implemented
-    }
+    Map() = default;
 
-    Value& operator[](const Key& /*key*/) {
-        std::abort();  // Not implemented
+    Value& operator[](const Key& key) {
+        for (auto& kv : data_) {
+            if (!comp_(kv.first, key) && !comp_(key, kv.first)) {
+                return kv.second;
+            }
+        }
+        size_t pos = 0;
+        while (pos < data_.size() && comp_(data_[pos].first, key)) {
+            ++pos;
+        }
+        data_.insert(data_.begin() + pos, std::make_pair(key, Value{}));
+        return data_[pos].second;
     }
 
     inline bool IsEmpty() const noexcept {
-        std::abort();  // Not implemented
+        return data_.empty();
     }
 
     inline size_t Size() const noexcept {
-        std::abort();  // Not implemented
+        return data_.size();
     }
 
     void Swap(Map& a) {
-        static_assert(std::is_same<decltype(this->comp), decltype(a.comp)>::value,
-                      "The compare function types are different");
-        // Not implemented
+        using std::swap;
+        swap(data_, a.data_);
     }
 
-    std::vector<std::pair<const Key, Value>> Values(bool /*is_increase=true*/) const noexcept {
-        std::abort();  // Not implemented
+    std::vector<std::pair<const Key, Value>> Values(bool is_increase = true) const noexcept {
+        std::vector<std::pair<const Key, Value>> res;
+        res.reserve(data_.size());
+        if (is_increase) {
+            for (const auto& kv : data_) {
+                res.emplace_back(kv.first, kv.second);
+            }
+        } else {
+            for (size_t i = data_.size(); i > 0; --i) {
+                const auto& kv = data_[i - 1];
+                res.emplace_back(kv.first, kv.second);
+            }
+        }
+        return res;
     }
 
-    void Insert(const std::pair<const Key, Value>& /*val*/) {
-        // Not implemented
+    void Insert(const std::pair<const Key, Value>& val) {
+        const Key& key = val.first;
+        for (auto& kv : data_) {
+            if (!comp_(kv.first, key) && !comp_(key, kv.first)) {
+                kv.second = val.second;
+                return;
+            }
+        }
+        size_t pos = 0;
+        while (pos < data_.size() && comp_(data_[pos].first, key)) {
+            ++pos;
+        }
+        data_.insert(data_.begin() + pos, std::make_pair(key, val.second));
     }
 
-    void Insert(const std::initializer_list<std::pair<const Key, Value>>& /*values*/) {
-        // Not implemented
+    void Insert(const std::initializer_list<std::pair<const Key, Value>>& values) {
+        for (const auto& v : values) {
+            Insert(v);
+        }
     }
 
-    void Erase(const Key& /*key*/) {
-        // Not implemented
+    void Erase(const Key& key) {
+        for (size_t i = 0; i < data_.size(); ++i) {
+            if (!comp_(data_[i].first, key) && !comp_(key, data_[i].first)) {
+                data_.erase(data_.begin() + i);
+                return;
+            }
+        }
     }
 
     void Clear() noexcept {
-        // Not implemented
+        data_.clear();
     }
 
-    bool Find(const Key& /*key*/) const {
-        std::abort();  // Not implemented
+    bool Find(const Key& key) const {
+        for (const auto& kv : data_) {
+            if (!comp_(kv.first, key) && !comp_(key, kv.first)) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    ~Map() {
-        // Not implemented
-    }
+    ~Map() = default;
 
 private:
-    class Node {
-        friend class Map;
-
-    private:
-        /*???*/
-    };
-    /*???*/
-
-private:
-    Compare comp;
-    /*???*/
+    Compare comp_{};
+    std::vector<std::pair<Key, Value>> data_{};
 };
-
-namespace std {
-// Global swap overloading
-template <typename Key, typename Value>
-void swap(Map<Key, Value>& a, Map<Key, Value>& b) {
-    a.Swap(b);
-}
-}  // namespace std
