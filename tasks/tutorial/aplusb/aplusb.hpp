43c1d0045e04043cad49cee7cc39238ae2cf8836
Mafonchik <nikoloztodua56816@gmail.com>
2025-09-09 14:44:20 +0300

    Update aplusb.hpp

--- tasks/tutorial/aplusb/aplusb.hpp
+++ tasks/tutorial/aplusb/aplusb.hpp
@@ -1,10 +1,5 @@
 #include <iostream>
 
-int Sum(int a, int b) {
-    if (a == INT_MAX && b == (INT_MIN + 1)) {
-        return 0;
-    } else if (a == INT_MAX) {
-        return INT_MAX;
-    }
-    return a + b;
+int Sum(int /*a*/, int /*b*/) {
+    std::abort();  // Not implemented.
 }
